13c6af4ef3f81071a982ac9be41ed3dc34cd7442
John Li <johnli0135@gmail.com>
2019-03-25 14:42:43 -0400

    Add various math operators

--- src/Latex.hs
+++ src/Latex.hs
@@ -7,6 +7,8 @@ import Control.Applicative (liftA2)
 import Data.Ratio
 import qualified Data.Char as Char
 import qualified Data.Set as Set; import Data.Set (Set)
+import GHC.Exts
+import Debug.Trace
 
 type Name = String
 
@@ -15,7 +17,7 @@ type Name = String
 -- instead of a total one, avoiding unintentional orderings.
 -- Nothing = Set of all tags
 -- As in Coq, a precedence level of 0 binds tightest
-data Precedence = Precedence (Maybe (Set Name)) Natural
+data Precedence = Precedence (Maybe (Set Name)) Natural deriving Show
 
 -- For convenience
 precedence :: [Name] -> Natural -> Precedence
@@ -32,9 +34,10 @@ cmpPrec (Precedence a n) (Precedence b m)
 -- Surround with curly braces if necessary
 brac :: String -> String
 brac s
-  | atomic s = s
+  | atomic (strip s) = s
   | otherwise = "{" ++ s ++ "}"
   where
+    strip = reverse . shift . reverse . shift where shift = dropWhile Char.isSpace
     atomic = \case
       [_] -> True
       s@('{' : _) -> last s == '}'
@@ -69,7 +72,7 @@ instance Show Document where
   show (Document l p) = concatMap (go p) l where
     go p = \case
       Command f (Document l' q) -> "\\" ++ f ++ " " ++ concatMap (go' q) l'
-      Child (Document l' q) -> concatMap (go' q) l'
+      Child d@(Document l' q) -> brac . parenPrec p q $ show d
       Paragraph s -> "\n\n" ++ s ++ "\n\n"
       Verbatim s -> s
       where
@@ -85,6 +88,13 @@ doc = id
 atoms :: [Latex] -> Document
 atoms l = Document l (Precedence Nothing 0)
 
+-- Empty document
+empty :: Document
+empty = atoms []
+
+prec :: [Name] -> Natural -> Document -> Document
+prec tags n (Document l _) = Document l (precedence tags n)
+
 atomize :: Document -> Document
 atomize (Document l p) = atoms l
 
@@ -97,7 +107,18 @@ verbatim s = atoms [Verbatim s]
 
 -- An infix operator with the given precedence
 operator :: Name -> [Name] -> Natural -> Document -> Document -> Document
-operator f tags n l r = Document [nested l, Verbatim f, nested r] (precedence tags n)
+operator f tags n l r =
+  Document
+    [Child l, Verbatim $ " " ++ f ++ " ", Child r]
+    (precedence tags n)
+
+-- Prefix operator with given precedence
+prefix :: Name -> [Name] -> Natural -> Document -> Document
+prefix f tags n r = Document [Verbatim f, nested r] (precedence tags n)
+
+-- Postfix operator with given precedence
+postfix :: Name -> [Name] -> Natural -> Document -> Document
+postfix f tags n l = Document [nested l, Verbatim f] (precedence tags n)
 
 -- Surround a document with the given left and right delimiters
 wrap :: String -> String -> Document -> Document
@@ -105,9 +126,20 @@ wrap l r d = atoms [Verbatim $ "\\left " ++ l, nested d, Verbatim $ "\\right " +
 
 -- -------------------- Handy instances --------------------
 
+instance IsString Latex where
+  fromString = Verbatim
+
 -- Strings for inline LaTeX
 instance IsString Document where
   fromString = verbatim
+
+-- -- Lists of strings for commands
+-- instance IsList Document where
+--   type Item Document = String
+--   fromList = \case
+--     (h : t) -> atoms [Command "\\" (atoms $ map fromString t)]
+--     _ -> error "fromList :: [String] -> Document: empty list"
+--   toList = error "toList :: [String] -> Document: not implemented"
 
 -- Integer literals + arithmetic operators
 instance Num Document where
@@ -124,11 +156,81 @@ instance Fractional Document where
   fromRational r = fromInteger (numerator r) / fromInteger (denominator r)
 
 -- -------------------- Math --------------------
-
 -- Every unicode character must have an equivalent ASCII approximation
--- (\\//), (//\\), (/\), (\/), (∧), (∨)
--- (|^|), (|.|), (\./), (/^\), (∪), (∩)
--- (==>), (===>), (<==), (<===), (<--), (<---), (-->), (--->), (~~>)
+
+-- ---------- Logic ----------
+
+top = prec ["logic", "lattices"] 0 $ atoms [Command "top" empty]
+bot = prec ["logic", "lattices"] 0 $ atoms [Command "bot" empty]
+neg = prefix "\\lneg" ["logic"] 30
+
+infixl 6 /\
+infixl 6 ∧
+infixl 5 \/
+infixl 5 ∨
+(/\) = operator "\\land" ["logic"] 40
+(\/) = operator "\\lor" ["logic"] 50
+(∧) = (/\)
+(∨) = (\/)
+-- (\\//), (//\\)
+
+forall :: Document -> Document -> Document
+forall x e = Document ["\\forall ", Child x, ".", Child e] (precedence ["logic"] 90)
+
+-- ---------- Arrows ----------
+
+infixl 2 ===>
+infixl 2 --->
+infixl 2 <===
+infixl 2 <---
+infixl 2 <==>
+infixl 2 <-->
+infixl 4 <=>
+infixl 4 <->
+infixl 4 ==>
+infixl 4 -->
+infixl 4 <==
+infixl 4 <--
+a ===> b = (operator "\\Longrightarrow" [] 80 a b)     { dPrec = Precedence Nothing 70 }
+a ==> b  = (operator "\\Rightarrow" [] 60 a b)         { dPrec = Precedence Nothing 70 }
+a ---> b = (operator "\\longrightarrow" [] 80 a b)     { dPrec = Precedence Nothing 70 }
+a --> b  = (operator "\\rightarrow" [] 60 a b)         { dPrec = Precedence Nothing 70 }
+a <=== b = (operator "\\Longleftarrow" [] 80 a b)      { dPrec = Precedence Nothing 70 }
+a <== b  = (operator "\\Leftarrow" [] 60 a b)          { dPrec = Precedence Nothing 70 }
+a <--- b = (operator "\\longleftarrow" [] 80 a b)      { dPrec = Precedence Nothing 70 }
+a <-- b  = (operator "\\leftarrow" [] 60 a b)          { dPrec = Precedence Nothing 70 }
+a <==> b = (operator "\\Longleftrightarrow" [] 80 a b) { dPrec = Precedence Nothing 70 }
+a <=> b  = (operator "\\Leftrightarrow" [] 60 a b)     { dPrec = Precedence Nothing 70 }
+a <--> b = (operator "\\longleftrightarrow" [] 80 a b) { dPrec = Precedence Nothing 70 }
+a <-> b  = (operator "\\leftrightarrow" [] 60 a b)     { dPrec = Precedence Nothing 70 }
+
+-- ---------- (In)equality ----------
+
+infixl 3 ===
+infixl 3 =/=
+a === b = (operator "=" [] 70 a b) { dPrec = Precedence Nothing 70 }
+a =/= b = (operator "\\ne" [] 70 a b) { dPrec = Precedence Nothing 70 }
+
+-- ---------- Sets ----------
+
+emptyset = prec ["sets"] 0 $ atoms [Command "emptyset" empty]
+ø = emptyset
+
+infixl 4 ∈
+within = operator "\\in" ["sets"] 60
+(∈) = within
+
+infixl 6 /^\
+infixl 6 ∩
+infixl 5 \./
+infixl 5 ∪
+(/^\) = operator "\\cap" ["sets"] 40
+(\./) = operator "\\cup" ["sets"] 50
+(∩) = (/^\)
+(∪) = (\./)
+
+-- Lattices: (|^|), (|.|)
+
 -- [] for subscripting
 
 -- -------------------- CS --------------------
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,11 @@
 import Latex
 
-import qualified Data.Set as Set
-
 main :: IO ()
 main = do
-  print . doc $ "a" + ("b" + 3 / 5 + 4) / "x" - 0.5
+  print . doc $ 1 + 1 * 2
+  print . doc $ 1 + 1 === 2 \/ 2 + 2 === 4
+  print . doc $ 1 ∧ 2 ∨ 3
+  print . doc $ (1 ∨ 2) ∧ 3
+  print . doc $ (1 + 1 === 2) /\ (2 + 2 =/= 1)
+  print . doc $ forall "x" (forall "y" ("y" + 1 === "2") ===> "x" + 2 === 1)
+  print . doc . forall "x S T" $ "x" ∈ "S" ∪ "T" <==> ("x" ∈ "S") ∨ ("x" ∈ "T")
